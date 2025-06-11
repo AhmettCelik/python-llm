bf6bc82aa66ee8873bb7c91c3c4c1732d0096cd1
AhmettCelik <140803207+AhmettCelik@users.noreply.github.com>
2025-06-11 15:25:25 +0300

    think like this commit never existed

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,46 +2,25 @@ import os
 
 
 def get_files_info(working_directory, directory=None):
-    try:
-        directory = os.path.join(working_directory, directory)
-        absolute_path = os.path.abspath(directory)
-        absolute_working_path = os.path.abspath(working_directory)
-    except Exception as e:
-        return f'Error: os.path.abspath throwing error: {e}'
-
-    if not absolute_path.startswith(absolute_working_path):
+    abs_working_dir = os.path.abspath(working_directory)
+    target_dir = abs_working_dir
+    if directory:
+        target_dir = os.path.abspath(
+            os.path.join(working_directory, directory))
+    if not target_dir.startswith(abs_working_dir):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-
-    if not os.path.isdir(absolute_path):
+    if not os.path.isdir(target_dir):
         return f'Error: "{directory}" is not a directory'
-
     try:
-        contents = os.listdir(absolute_path)
+        files_info = []
+        for filename in os.listdir(target_dir):
+            filepath = os.path.join(target_dir, filename)
+            file_size = 0
+            is_dir = os.path.isdir(filepath)
+            file_size = os.path.getsize(filepath)
+            files_info.append(
+                f"- {filename}: file_size={file_size} bytes, is_dir={is_dir}"
+            )
+        return "\n".join(files_info)
     except Exception as e:
-        return f"Error: os.listdir throwing error: {e}"
-
-    contents_data = {}
-
-    try:
-        for content in contents:
-            content_path = os.path.join(absolute_path, content)
-            contents_data[content] = {}
-            content_size = os.path.getsize(content_path)
-            contents_data[content]["file_size"] = content_size
-
-            if os.path.isdir(content_path):
-                contents_data[content]["is_dir"] = True
-            else:
-                contents_data[content]["is_dir"] = False
-    except Exception as e:
-        return f"Error: an error occured in contents list for loop {e}"
-
-    result_lines = []
-    for content, data in contents_data.items():
-        line = f"- {content}: file_size={data['file_size']
-                                         } bytes, is_dir={data['is_dir']}"
-        result_lines.append(line)
-
-        result_string = "\n".join(result_lines)
-
-    return result_string
+        return f"Error listing files: {e}"
